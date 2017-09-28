faef2760566c0e33c6ac7c2a4c41b1a504ad58a4
Dennis Walsh <lawjolla@aol.com>
2017-09-28 14:01:43 -0700

    Update 9/28/17

--- cache-dir/static-entry.js
+++ cache-dir/static-entry.js
@@ -2,7 +2,7 @@ import React from "react"
 import { renderToString, renderToStaticMarkup } from "react-dom/server"
 import { StaticRouter, Route, withRouter } from "react-router-dom"
 import { kebabCase, get, merge, isArray, isString } from "lodash"
-import { apiRunnerAsync } from "./api-runner-ssr"
+import apiRunner from "./api-runner-ssr"
 import pages from "./pages.json"
 import syncRequires from "./sync-requires"
 import testRequireError from "./test-require-error"
@@ -15,9 +15,9 @@ try {
     Html = require(`./default-html`)
   } else {
     console.log(
-      `\n\nThere was an error requiring "src/html.js"\n\n`,
-      err,
-      `\n\n`
+        `\n\nThere was an error requiring "src/html.js"\n\n`,
+        err,
+        `\n\n`
     )
     process.exit()
   }
@@ -29,17 +29,14 @@ const pathChunkName = path => {
 }
 
 const getPage = path => pages.find(page => page.path === path)
-const defaultLayout = props =>
-  <div>
-    {props.children()}
-  </div>
+const defaultLayout = props => <div>{props.children()}</div>
 
 const getLayout = page => {
   const layout = syncRequires.layouts[page.layoutComponentChunkName]
   return layout ? layout : defaultLayout
 }
 
-const $ = React.createElement
+const createElement = React.createElement
 
 module.exports = (locals, callback) => {
   let pathPrefix = `/`
@@ -73,28 +70,32 @@ module.exports = (locals, callback) => {
     bodyProps = merge({}, bodyProps, props)
   }
 
-  const bodyComponent = $(
-    StaticRouter,
-    {
-      location: {
-        pathname: locals.path,
+  const bodyComponent = createElement(
+      StaticRouter,
+      {
+        location: {
+          pathname: locals.path,
+        },
+        context: {},
       },
-      context: {},
-    },
-    $(Route, {
-      render: props => {
-        const page = getPage(props.location.pathname)
-        const layout = getLayout(page)
-        return $(withRouter(layout), {
-          ...props,
-          children: props =>
-            $(syncRequires.components[page.componentChunkName], {
-              ...props,
-              ...syncRequires.json[page.jsonName],
-            }),
-        })
-      },
-    })
+      createElement(Route, {
+        render: routeProps => {
+          const page = getPage(routeProps.location.pathname)
+          const layout = getLayout(page)
+          return createElement(withRouter(layout), {
+            children: layoutProps => {
+              const props = layoutProps ? layoutProps : routeProps
+              return createElement(
+                  syncRequires.components[page.componentChunkName],
+                  {
+                    ...props,
+                    ...syncRequires.json[page.jsonName],
+                  }
+              )
+            },
+          })
+        },
+      })
   )
   apiRunnerAsync('replaceRenderer',
       ({
@@ -121,96 +122,96 @@ module.exports = (locals, callback) => {
       }
 
 
-      // Add the chunk-manifest as a head component.
-      const chunkManifest = require(`!raw!../public/chunk-manifest.json`)
+// Add the chunk-manifest as a head component.
+    const chunkManifest = require(`!raw!../public/chunk-manifest.json`)
 
-      headComponents.unshift(
-          <script
-              id="webpack-manifest"
-              key="webpack-manifest"
-              dangerouslySetInnerHTML={{
-                __html: `
+    headComponents.unshift(
+        <script
+            id="webpack-manifest"
+            key="webpack-manifest"
+            dangerouslySetInnerHTML={{
+              __html: `
             //<![CDATA[
             window.webpackManifest = ${chunkManifest}
             //]]>
             `,
-              }}
-          />
+            }}
+        />
+    )
+
+    let stats
+    try {
+      stats = require(`../public/stats.json`)
+    } catch (e) {
+      // ignore
+    }
+
+    // Create paths to scripts
+    const page = pages.find(page => page.path === locals.path)
+    const scripts = [
+      `commons`,
+      `app`,
+      pathChunkName(locals.path),
+      page.componentChunkName,
+      page.layoutComponentChunkName,
+    ]
+        .map(s => {
+          const fetchKey = `assetsByChunkName[${s}]`
+
+          let fetchedScript = get(stats, fetchKey)
+
+          if (!fetchedScript) {
+            return null
+          }
+
+          // If sourcemaps are enabled, then the entry will be an array with
+          // the script name as the first entry.
+          fetchedScript = isArray(fetchedScript) ? fetchedScript[0] : fetchedScript
+          const prefixedScript = `${pathPrefix}${fetchedScript}`
+
+          // Make sure we found a component.
+          if (prefixedScript === `/`) {
+            return null
+          }
+
+          return prefixedScript
+        })
+        .filter(s => isString(s))
+
+    scripts.forEach(script => {
+      // Add preload <link>s for scripts.
+      headComponents.unshift(
+          <link rel="preload" key={script} href={script} as="script" />
       )
+    })
 
-      let stats
-      try {
-        stats = require(`../public/stats.json`)
-      } catch (e) {
-        // ignore
-      }
-
-      // Create paths to scripts
-      const page = pages.find(page => page.path === locals.path)
-      const scripts = [
-        `commons`,
-        `app`,
-        pathChunkName(locals.path),
-        page.componentChunkName,
-        page.layoutComponentChunkName,
-      ]
-          .map(s => {
-            const fetchKey = `assetsByChunkName[${s}]`
-
-            let fetchedScript = get(stats, fetchKey)
-
-            if (!fetchedScript) {
-              return null
-            }
-
-            // If sourcemaps are enabled, then the entry will be an array with
-            // the script name as the first entry.
-            fetchedScript = isArray(fetchedScript) ? fetchedScript[0] : fetchedScript
-            const prefixedScript = `${pathPrefix}${fetchedScript}`
-
-            // Make sure we found a component.
-            if (prefixedScript === `/`) {
-              return null
-            }
-
-            return prefixedScript
-          })
-          .filter(s => isString(s))
-
-      scripts.forEach(script => {
-        // Add preload <link>s for scripts.
-        headComponents.unshift(
-            <link rel="preload" key={script} href={script} as="script"/>
-        )
-      })
-
-      // Add script loader for page scripts to the head.
-      // Taken from https://www.html5rocks.com/en/tutorials/speed/script-loading/
-      const scriptsString = scripts.map(s => `"${s}"`).join(`,`)
-      headComponents.push(
-          <script
-              key={`script-loader`}
-              dangerouslySetInnerHTML={{
-                __html: `
+    // Add script loader for page scripts to the head.
+    // Taken from https://www.html5rocks.com/en/tutorials/speed/script-loading/
+    const scriptsString = scripts.map(s => `"${s}"`).join(`,`)
+    headComponents.push(
+        <script
+            key={`script-loader`}
+            dangerouslySetInnerHTML={{
+              __html: `
   !function(e,t,r){function n(){for(;d[0]&&"loaded"==d[0][f];)c=d.shift(),c[o]=!i.parentNode.insertBefore(c,i)}for(var s,a,c,d=[],i=e.scripts[0],o="onreadystatechange",f="readyState";s=r.shift();)a=e.createElement(t),"async"in i?(a.async=!1,e.head.appendChild(a)):i[f]?(d.push(a),a[o]=n):e.write("<"+t+' src="'+s+'" defer></'+t+">"),a.src=s}(document,"script",[
   ${scriptsString}
 ])
   `,
-              }}
-          />
-      )
+            }}
+        />
+    )
 
-      const html = `<!DOCTYPE html>\n ${renderToStaticMarkup(
-          <Html
-              {...bodyProps}
-              headComponents={headComponents}
-              preBodyComponents={preBodyComponents}
-              postBodyComponents={postBodyComponents}
-              body={bodyHTML}
-              path={locals.path}
-          />
-      )}`
+    const html = `<!DOCTYPE html>\n ${renderToStaticMarkup(
+        <Html
+            {...bodyProps}
+            headComponents={headComponents}
+            preBodyComponents={preBodyComponents}
+            postBodyComponents={postBodyComponents}
+            body={bodyHTML}
+            path={locals.path}
+        />
+    )}`
 
-      callback(null, html)
-    })
+    callback(null, html)
+  })
 }
